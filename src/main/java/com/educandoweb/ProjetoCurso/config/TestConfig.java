ba18320601954521631f1900eade410104f568b0
Igor Raphael <igor.raphael.sc.28@gmail.com>
2026-06-06 15:34:30 -0300

    Category

--- src/main/java/com/educandoweb/ProjetoCurso/config/TestConfig.java
+++ src/main/java/com/educandoweb/ProjetoCurso/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.educandoweb.ProjetoCurso.config;
 
+import com.educandoweb.ProjetoCurso.entities.Category;
 import com.educandoweb.ProjetoCurso.entities.Order;
 import com.educandoweb.ProjetoCurso.entities.User;
 import com.educandoweb.ProjetoCurso.enums.OrderStatus;
+import com.educandoweb.ProjetoCurso.repositores.CategoryRepository;
 import com.educandoweb.ProjetoCurso.repositores.OrderRepository;
 import com.educandoweb.ProjetoCurso.repositores.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,7 +26,17 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Override
     public void run(String... args) throws Exception{
+
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
---
+++ src/main/java/com/educandoweb/ProjetoCurso/repositores/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.educandoweb.ProjetoCurso.repositores;
+
+import com.educandoweb.ProjetoCurso.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
