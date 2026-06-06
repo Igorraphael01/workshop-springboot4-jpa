3a2bbcf342e9587fc923e63d4419c53c699771c6
Igor Raphael <igor.raphael.sc.28@gmail.com>
2026-06-06 15:17:37 -0300

    OrderStatus enum

--- src/main/java/com/educandoweb/ProjetoCurso/entities/Order.java
+++ src/main/java/com/educandoweb/ProjetoCurso/entities/Order.java
@@ -1,5 +1,6 @@
 package com.educandoweb.ProjetoCurso.entities;
 
+import com.educandoweb.ProjetoCurso.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -23,6 +24,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -31,10 +34,11 @@ public class Order implements Serializable {
         }
 
 
-    public Order(Long id, Instant moment, User cliente) {
+    public Order(Long id, Instant moment,OrderStatus orderStatus, User cliente) {
             super();
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = cliente;
     }
 
@@ -60,6 +64,15 @@ public class Order implements Serializable {
 
     public void setCliente(User cliente) {
         this.client = cliente;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+            if(orderStatus != null)
+        this.orderStatus = orderStatus.getCode();
     }
 
     @Override
